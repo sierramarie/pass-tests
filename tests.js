99dec46d953001eaf6b67d75a40540ed579ded91
Sierra <sierramroberts.96@gmail.com>
2017-02-16 16:09:22 -0500

    Finish

--- tests.js
+++ tests.js
@@ -51,7 +51,6 @@ avow('repeatWord returns a repeated word', repeatWord('Knock.', 2) === 'Knock.Kn
 console.log(repeatWord('I want french fries.', 5))
 
 function sliceWord(value, n1, n2) {
-    var value = 'I love pizza!'
     return value.slice(7, 12)
 }
 
@@ -76,44 +75,58 @@ function doesItContain(value, match) {
     return value.includes(match)
 }
 
-
 avow('doesItContain returns true if it finds a match', doesItContain('The Children\'s Museum Of Indianapolis', 'Museum') === true);
 
 // Regular Expressions
 function extractZipCode(value) {
+    return value.match(/\d/g).join('')
     
 }
 
 avow('extractZipCode returns a zip code', extractZipCode('My zip code was 46240.') === '46240');
-
-function extractPhoneNumber() {
+function extractPhoneNumber(value) {
+    return value.match(/\d/g).join('')
 }
 
 avow('extractPhoneNumber returns a string of numbers', extractPhoneNumber('(888) 123.1234') === '8881231234');
 
 // Numbers
-function stringToNumber() {
+function stringToNumber(value) {
+    return Number(value);
 }
 
 avow('stringToNumber returns a proper Number', stringToNumber('1999') === 1999);
 
-function incrementByOne() {
+function incrementByOne(value) {
+    value++;
+
+    return value;
 }
 
 avow('incrementByOne returns a number one higher', incrementByOne(0) === 1);
 
 function isDivisibleByThree(value) {
-    return value === 0
+    if (value % 3 === 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
 avow('isDivisibleByThree returns true if number is divisible by 3', isDivisibleByThree(9) === true);
 
-function makeRandomNumber() {
+function makeRandomNumber(value) {
+    var value = Math.random(10);
+    return value;
+
 }
 
 avow('makeRandomNumber returns a random number between 0 and 10', makeRandomNumber(10) >= 0 && makeRandomNumber(10) <= 10);
 
-function roundNumber() {
+function roundNumber(value) {
+    var value = Math.round(5.51);
+    return value;
 }
 
 avow('roundNumber returns a number rounded up', roundNumber(5.51) === 6);
@@ -121,18 +134,22 @@ avow('roundNumber returns a number rounded up', roundNumber(5.51) === 6);
 // Dates
 function justAMoment() {
     // Look at moment.js docs to find how to return the day name, like Wednesday
+    var value = moment().format('dddd');
+    return value;
 }
 
 avow('justAMoment returns true if the function returns a day name', justAMoment().includes('day') === true);
 
 // Objects
-function myNameIs() {
+function myNameIs(value) {
+    return value.name
 }
 
 avow('myNameIs returns your name from an object with a name property', myNameIs({name:'Your Name Here'}) === 'Your Name Here');
 
 // Arrays
-function getFirstItem() {
+function getFirstItem(dog) {
+    return dog[0];
 }
 
 avow('getFirstItem returns the first item in the array', getFirstItem(['Dog','Cat','Pig','Bird','Fish']) === 'Dog')
