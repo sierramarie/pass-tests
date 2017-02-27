76702e18e87effe9024ef7c2ceecf6435e8b2af9
Sierra <sierramroberts.96@gmail.com>
2017-02-27 13:57:16 -0500

    Fixed

--- tests.js
+++ tests.js
@@ -7,8 +7,10 @@ function isTrue(value) {
 }
 
 avow('isTrue returns true', isTrue(1 + 1 === 2) === true);
+// ------------
 
 // Null & Undefined
+
 function isNull(value) {
     if (value === null) {
         return true;
@@ -17,6 +19,7 @@ function isNull(value) {
 
 var emptyGlass = null;
 avow('isNull returns true', isNull(emptyGlass) === true);
+// -----------
 
 function isUndefined(value) {
     if (value === undefined) {
@@ -26,14 +29,17 @@ function isUndefined(value) {
 
 var blockOfClay;
 avow('isUndefined returns true', isUndefined(blockOfClay) === true);
+// ----------
 
 // Strings
+
 function convertCaseLower(value) {
     var value = value.toLowerCase();
     return value;
 }
 
 avow('convertCaseLower returns a lowercase string', convertCaseLower('I AM WHISPERING.') === 'i am whispering.');
+// ------------
 
 function convertCaseUpper(value) {
     var value = value.toUpperCase();
@@ -41,6 +47,7 @@ function convertCaseUpper(value) {
 }
 
 avow('convertCaseUpper returns an uppercase string', convertCaseUpper('i am shouting!') === 'I AM SHOUTING!');
+// --------------
 
 function repeatWord(value, times) {
     return value.repeat(times)
@@ -48,12 +55,14 @@ function repeatWord(value, times) {
 
 avow('repeatWord returns a repeated word', repeatWord('Knock.', 2) === 'Knock.Knock.');
 console.log(repeatWord('I want french fries.', 5))
+// ------------
 
 function sliceWord(value, n1, n2) {
     return value.slice(n1, n2)
 }
 
 avow('sliceWord returns a slice of a word', sliceWord('I love pizza!', 7, 12) === 'pizza');
+// -----------
 
 function splitWord(value, howToSplit) {
     // var value = 'US,CA,MX,JP,UK'
@@ -63,38 +72,47 @@ function splitWord(value, howToSplit) {
 }
 
 avow('splitWord returns an array', splitWord('US,CA,MX,JP,UK', ',').length === 5);
+// -----------
 
 function whereIsCarmen(value, name) {
     return value.indexOf(name)
 }
 
 avow('whereIsCarmen returns Carmen\'s position', whereIsCarmen('In the rainy Panama Canal, Carmen Sandiego silently slipped aboard a rusty shipping container bound for New Zealand.', 'Carmen') === 27);
+// ----------------
 
 function doesItContain(value, match) {
     return value.includes(match)
 }
 
 avow('doesItContain returns true if it finds a match', doesItContain('The Children\'s Museum Of Indianapolis', 'Museum') === true);
+// ------------
 
 // Regular Expressions
+
 function extractZipCode(value) {
     return value.match(/\d/g).join('')
     
 }
 
 avow('extractZipCode returns a zip code', extractZipCode('My zip code was 46240.') === '46240');
+// -----------
+
 function extractPhoneNumber(value) {
     return value.match(/\d/g).join('')
 }
 
 avow('extractPhoneNumber returns a string of numbers', extractPhoneNumber('(888) 123.1234') === '8881231234');
+// ---------
 
 // Numbers
+
 function stringToNumber(value) {
     return Number(value);
 }
 
 avow('stringToNumber returns a proper Number', stringToNumber('1999') === 1999);
+// ------------
 
 function incrementByOne(value) {
     value++;
@@ -103,6 +121,7 @@ function incrementByOne(value) {
 }
 
 avow('incrementByOne returns a number one higher', incrementByOne(0) === 1);
+// ---------
 
 function isDivisibleByThree(value) {
     if (value % 3 === 0) {
@@ -115,17 +134,22 @@ function isDivisibleByThree(value) {
 
 avow('isDivisibleByThree returns true if number is divisible by 3', isDivisibleByThree(9) === true);
 
-function makeRandomNumber(min, max) {
-  return Math.random() * (10 - 0) + 0;
-    }
+// ------------------
+
+function makeRandomNumber(value) {
+    return Math.round(Math.random(value) * 10);
+}
 avow('makeRandomNumber returns a random number between 0 and 10', makeRandomNumber(10) >= 0 && makeRandomNumber(10) <= 10);
 
+// i had it hard coded so the only numbers it could return were between 0 - 10 but if for some reason the data needed to change it wouldnt be able to because i had a min & a max set. using (value) * 10 returns a number between 1 and 10 randomly every time. 
+// -------------
 function roundNumber(value) {
     var value = Math.round(6);
     return value;
 }
 
 avow('roundNumber returns a number rounded up', roundNumber(5.51) === 6);
+// ----------------
 
 // Dates
 function justAMoment() {
@@ -135,15 +159,19 @@ function justAMoment() {
 }
 
 avow('justAMoment returns true if the function returns a day name', justAMoment().includes('day') === true);
+// --------------
 
 // Objects
+
 function myNameIs(value) {
     return value.name
 }
 
 avow('myNameIs returns your name from an object with a name property', myNameIs({name:'Your Name Here'}) === 'Your Name Here');
+// -----------
 
 // Arrays
+
 function getFirstItem(dog) {
     return dog[0];
 }
